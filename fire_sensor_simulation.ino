24ce6ab6265e607b73fb3ecb1b4a7079eb1211eb
Kujoh-20 <johninielthomas@gmail.com>
2025-10-06 20:21:43 +0800

    Add files via upload

---
+++ fire_sensor_simulation.ino
@@ -0,0 +1,51 @@
+//define PINS
+#define thermistor_Pin A0
+#define photo_Pin A2
+#define alert_Pin 12
+
+//thershold
+const float temp_Thershold = 50.0;
+const int light_Thershold = 220;
+
+const int beta = 3950;
+const int resistance = 10;
+
+void setup() {
+  pinMode(alert_Pin, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+
+  float temperature = readTemperature();
+  int brightness = readBrightness();
+
+  Serial.print("Temp: ");
+  Serial.print(temperature);
+  Serial.print(" °C | Brightness: ");
+  Serial.println(brightness);
+
+  if (temperature >= temp_Thershold && brightness <= light_Thershold) {
+    digitalWrite(alert_Pin, HIGH);
+    delay(100);
+    digitalWrite(alert_Pin, LOW);
+    delay(100);
+  } else {
+    digitalWrite(alert_Pin, LOW);
+  }
+
+  delay(500);
+}
+
+float readTemperature() {
+  int analogValue = analogRead(thermistor_Pin);
+
+  float tempC = beta / (log((1025.0 * resistance / analogValue - resistance) / resistance) + beta / 298.0) - 273.0;
+
+  return tempC;
+}
+
+int readBrightness() {
+  int brightness = analogRead(photo_Pin);
+  return brightness;
+}
