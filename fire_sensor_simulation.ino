7ed5c00720b68482da775b0bcaa3c8971702cb90
Kujoh-20 <johninielthomas@gmail.com>
2025-10-11 08:39:26 +0800

    Update fire_sensor_simulation.ino

--- fire_sensor_simulation.ino
+++ fire_sensor_simulation.ino
@@ -1,21 +1,21 @@
-// Define PINS
-#define thermistor_Pin A0
-#define photo_Pin A2
-#define alert_Pin 12
+  // Define PINS
+  #define thermistor_Pin A0
+  #define photo_Pin A2
+  #define alert_Pin 12
 
-// Thresholds
-const float temp_Threshold = 50.0; // Celsius
-const int light_Threshold = 220;   // Brightness
+  // Thresholds
+  const float temp_Threshold = 50.0; // Celsius
+  const int light_Threshold = 220;   // Brightness
 
-const int beta = 3950;
-const int resistance = 10000; // 10k ohm fixed resistor
+  const int beta = 3950;
+  const int resistance = 10; // 10k ohm fixed resistor
 
-void setup() {
+  void setup() {
   pinMode(alert_Pin, OUTPUT);
   Serial.begin(9600);
-}
+  }
 
-void loop() {
+  void loop() {
   float temperature = readTemperature();
   int brightness = readBrightness();
 
@@ -25,25 +25,23 @@ void loop() {
   Serial.println(brightness);
 
   if (temperature >= temp_Threshold && brightness >= light_Threshold) {
-    digitalWrite(alert_Pin, HIGH);
-    delay(100);
-    digitalWrite(alert_Pin, LOW);
-    delay(100);
+  digitalWrite(alert_Pin, HIGH);
+  delay(100);
+  digitalWrite(alert_Pin, LOW);
+  delay(100);
   } else {
-    digitalWrite(alert_Pin, LOW);
+  digitalWrite(alert_Pin, LOW);
   }
 
   delay(500);
-}
+  }
 
-float readTemperature() {
+  float readTemperature() {
   int analogValue = analogRead(thermistor_Pin);
-  float R = (1023.0 / analogValue - 1.0) * resistance; // Thermistor resistance
-  float tempK = 1.0 / (log(R / resistance) / beta + 1.0 / 298.15);
-  float tempC = tempK - 273.15;
+  float tempC = beta / (log(1023.0 * resistance / analogValue - resistance) + beta / 298.0) - 273.0;
   return tempC;
-}
+  }
 
-int readBrightness() {
+  int readBrightness() {
   return analogRead(photo_Pin);
-}
+  }
